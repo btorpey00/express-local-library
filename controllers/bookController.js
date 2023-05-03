ce3db1f480d78b51bae682d5dcfdf721ec33b4a1
btorpey00 <120498601+btorpey00@users.noreply.github.com>
2023-05-03 18:44:49 -0400

    create pages for all books, authors, genres, and instances

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -3,7 +3,7 @@ const Author = require('../models/author');
 const Genre = require('../models/genre');
 const BookInstance = require('../models/bookinstance');
 
-const { body, validationResult } = require('express-validator');
+// const { body, validationResult } = require('express-validator');
 const asyncHandler = require('express-async-handler');
 
 exports.index = asyncHandler(async (req, res, next) => {
@@ -32,7 +32,12 @@ exports.index = asyncHandler(async (req, res, next) => {
 });
 
 exports.book_list = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Book list');
+    const allBooks = await Book.find({}, 'title author')
+        .sort({ title: 1 })
+        .populate('author')
+        .exec();
+
+        res.render('book_list', { title: 'Book List', book_list: allBooks });
 });
 
 exports.book_detail = asyncHandler(async (req, res, next) => {
--- models/bookinstance.js
+++ models/bookinstance.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const { DateTime } = require('luxon');
+
 const BookInstanceSchema = new Schema({
     book: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
     imprint: { type: String, required: true },
@@ -17,5 +19,9 @@ const BookInstanceSchema = new Schema({
 BookInstanceSchema.virtual('url').get(function() {
     return `/catalog/bookinstance/${this._id}`;
 });
+
+BookInstanceSchema.virtual('due_back_formatted').get(function() {
+    return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+})
 
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
