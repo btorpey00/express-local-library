bcf4f03712b2da7c36b9335a3f824f7aa9fce282
btorpey00 <120498601+btorpey00@users.noreply.github.com>
2023-05-03 17:53:02 -0400

    add route controllers

---
+++ controllers/bookController.js
@@ -0,0 +1,64 @@
+const Book = require('../models/book');
+const Author = require('../models/author');
+const Genre = require('../models/genre');
+const BookInstance = require('../models/bookinstance');
+
+const { body, validationResult } = require('express-validator');
+const asyncHandler = require('express-async-handler');
+
+exports.index = asyncHandler(async (req, res, next) => {
+    const [
+        numBooks,
+        numBookInstances,
+        numAvailableBookInstances,
+        numAuthors,
+        numGenres,
+    ] = await Promise.all([
+        Book.countDocuments({}).exec(),
+        BookInstance.countDocuments({}).exec(),
+        BookInstance.countDocuments({ status: 'Available' }).exec(),
+        Author.countDocuments({}).exec(),
+        Genre.countDocuments({}).exec(),
+    ]);
+
+    res.render('index', { 
+        title: 'Local Library Home',
+        book_count: numBooks,
+        book_instance_count: numBookInstances,
+        book_instance_available_count: numAvailableBookInstances,
+        author_count: numAuthors,
+        genre_count: numGenres,
+    });
+});
+
+exports.book_list = asyncHandler(async (req, res, next) => {
+    res.send('NOT IMPLEMENTED: Book list');
+});
+
+exports.book_detail = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: Book detail: ${req.params.id}`);
+});
+
+exports.book_create_get = asyncHandler(async (req, res, next) => {
+    res.send('NOT IMPLEMENTED: Book create GET');
+});
+
+exports.book_create_post = asyncHandler(async (req, res, next) => {
+    res.send('NOT IMPLEMENTED: Book create POST');
+});
+
+exports.book_delete_get = asyncHandler(async (req, res, next) => {
+    res.send('NOT IMPLEMENTED: Book delete GET');
+});
+
+exports.book_delete_post = asyncHandler(async (req, res, next) => {
+    res.send('NOT IMPLEMENTED: Book delete POST');
+});
+
+exports.book_update_get = asyncHandler(async (req, res, next) => {
+    res.send('NOT IMPLEMENTED: Book update GET');
+});
+
+exports.book_update_post = asyncHandler(async (req, res, next) => {
+    res.send('NOT IMPLEMENTED: Book update POST');
+});
